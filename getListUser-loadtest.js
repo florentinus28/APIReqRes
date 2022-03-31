0fa3bfc2fb010f6aed4368c9ecdc30cc65c0b472
florentinus28 <florentinusaansetiawan@gmail.com>
2022-03-31 12:01:51 +0700

    add vus

--- getListUser-loadtest.js
+++ getListUser-loadtest.js
@@ -2,8 +2,8 @@ import { group } from 'k6';
 import { getListUser } from './Test function/getListUser.js';
 
 export const options = {
-    vus: 1,
-    duration: '1s',
+    vus: 10,
+    duration: '10s',
     thresholds: {
       http_req_failed: ['rate<0.01'], // http errors should be less than 1%
       http_req_duration: ['p(95)<500'], // 95% of requests should be below 200ms
