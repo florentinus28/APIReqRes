14b5b9b2f68b07e6c9c2de6fdf6e98684bdd07b8
florentinus28 <florentinusaansetiawan@gmail.com>
2022-04-01 15:10:44 +0700

    add report html

--- login-validationTest.js
+++ login-validationTest.js
@@ -1,5 +1,6 @@
 import { check,group } from 'k6';
 import http from "k6/http";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { login } from './Test function/login.js';
 
 export default function main () {
@@ -8,6 +9,7 @@ export default function main () {
     let reqBody;
     let reqHeader;
     let accessToken;
+    let errMsg;
 
     group ("Login with valid params", function(){
         const email = 'eve.holt@reqres.in';
@@ -24,7 +26,7 @@ export default function main () {
         // console.log(`Access Token is : ${accessToken}`)
         check(response,{
             'Check Response : Login status is 200': (r) => r.status === 200,
-            'Check Data : Token is not empty' : (r) => r.json()['token'] !== null,
+            'Check Data : Token is not empty' : (r) => r.json()['token'] !== null && accessToken !==undefined && accessToken !== '',
             'Check Data Type : Token is String' : (r) => typeof r.json()['token'] === 'string',
             'Check Data Length : Length data of Token is 17 char' : (r) => r.json()['token'].length === 17
         })
@@ -44,7 +46,7 @@ export default function main () {
         check(response,{
             'Check Response : Login status is 200': (r) => r.status === 200,
             'Check Data : Token is not empty' : (r) => r.json()['token'] !== null,
-            'Check Data : Error is undefined' : (r) => r.json()['error'] === undefined,
+            'Check Data : Error is undefined' : (r) => r.json()['error'] === undefined || errMsg === null || errMsg === '',
             'Check Data Type : Token is String' : (r) => typeof r.json()['token'] === 'string',
             'Check Data Length : Length data of Token is 17 char' : (r) => r.json()['token'].length === 17
         })
@@ -61,10 +63,11 @@ export default function main () {
         
         response = http.post(reqUrl,reqBody,reqHeader)
         // console.log(response.body)
+        errMsg = response.json()['error']
         check(response,{
             'Check Response : Login status is 400': (r) => r.status === 400,
             'Check Data : Token is undefined' : (r) => r.json()['token'] === undefined,
-            'Check Data : Error is not empty' : (r) => r.json()['error'] !== null,
+            'Check Data : Error is not empty' : (r) => r.json()['error'] !== undefined && errMsg !== null && errMsg !== '',
             'Check Data Type : Error is String' : (r) => typeof r.json()['error'] === 'string',
             'Check Data Length : Length data of Error is less than equal 100 char' : (r) => r.json()['error'].length <= 100,
             'Check error message : The error message is : user not found' : (r) => r.json()['error']==='user not found'
@@ -155,3 +158,9 @@ export default function main () {
         })
     })
 }
+let testTime=date.now()
+export function handleSummary(data) {
+    return {
+      'testResult.html': htmlReport(data),
+    };
+  }
